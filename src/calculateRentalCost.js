ab0d5ccaccab9d1be76137ade6d26ee168cf657c
MaksSavchenko <savchenkomaks96@gmail.com>
2025-04-03 12:07:51 +0300

    Fixed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,15 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  const DAILY_RATE = 40;
+  const MEDIUM_TERM_DISCOUNT = 20;
+  const LONG_TERM_DISCOUNT = 50;
+
   if (days >= 7) {
-    return days * 40 - 50;
+    return days * DAILY_RATE - LONG_TERM_DISCOUNT;
   }
 
   if (days >= 3) {
-    return days * 40 - 20;
+    return days * DAILY_RATE - MEDIUM_TERM_DISCOUNT;
   }
 
-  return days * 40;
+  return days * DAILY_RATE;
 }
 
 module.exports = calculateRentalCost;
